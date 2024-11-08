c468c8887cd405e82193823beec99caabca8ee0a
Nikolai Rodionov <nikolai.rodionov@onpier.de>
2024-11-08 23:58:46 +0100

    Fix the test script

--- bin/run-unit-tests.pl
+++ bin/run-unit-tests.pl
@@ -9,8 +9,6 @@ foreach my $test (<./unit-tests/*>) {
   print $test . "\n";
   foreach my $case (<$test/tests/*.yaml>) {
     $case =~ s/$test\///;
-    system("helm dependency build $test") == 0
-        or die "Couldn't build dependencies for $test";
     system("helm dependency update $test") == 0
         or die "Couldn't update dependencies for $test";
     push(@failed, "$test/$case") if system("helm unittest $test -f $case") != 0;
